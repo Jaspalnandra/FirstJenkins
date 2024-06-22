ca87bb7bcca67a35764823f6a2b95275dc8bc5b6
unknown <jaspal.nandra1990@gmail.com>
2024-06-23 00:23:02 +0530

    second-commit

--- Automatemavenjenkins/src/test/java/com/automatemavenjenkins/Automatemavenjenkins/FirstLaunchDemo.java
+++ Automatemavenjenkins/src/test/java/com/automatemavenjenkins/Automatemavenjenkins/FirstLaunchDemo.java
@@ -10,17 +10,20 @@ import org.testng.annotations.Test;
 public class FirstLaunchDemo {
 	static WebDriver driver;
 	String url;
-	
-	@BeforeSuite
-	public void setup() {
-		System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");		
-		driver = new FirefoxDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-	}
+//	
+//	@BeforeSuite
+//	public void setup() {
+//		System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");		
+//		driver = new FirefoxDriver();
+//		driver.manage().window().maximize();
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+//	}
 	
 	@Test
 	public void launch() {
+		System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");		
+		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
 		url = "https://www.google.com";	
 		driver.get(url);
 		System.out.println("RUNNNNNNNNNNNN");
