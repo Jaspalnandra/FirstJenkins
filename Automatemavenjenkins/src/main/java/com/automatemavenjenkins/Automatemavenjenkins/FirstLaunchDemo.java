8636d187f51cab7bae469ad5cf979aebd5fad0ec
unknown <jaspal.nandra1990@gmail.com>
2024-06-22 15:13:24 +0530

    Third-commit

--- Automatemavenjenkins/src/main/java/com/automatemavenjenkins/Automatemavenjenkins/FirstLaunchDemo.java
+++ Automatemavenjenkins/src/main/java/com/automatemavenjenkins/Automatemavenjenkins/FirstLaunchDemo.java
@@ -3,10 +3,8 @@ package com.automatemavenjenkins.Automatemavenjenkins;
 import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class FirstLaunchDemo {
