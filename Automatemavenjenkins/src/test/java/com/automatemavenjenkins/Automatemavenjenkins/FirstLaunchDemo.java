eee3412545dfc57164395b7880d5a3690d88578b
unknown <jaspal.nandra1990@gmail.com>
2024-06-23 17:14:50 +0530

    second-commit

--- Automatemavenjenkins/src/test/java/com/automatemavenjenkins/Automatemavenjenkins/FirstLaunchDemo.java
+++ Automatemavenjenkins/src/test/java/com/automatemavenjenkins/Automatemavenjenkins/FirstLaunchDemo.java
@@ -4,28 +4,34 @@ import java.time.Duration;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 public class FirstLaunchDemo {
 	static WebDriver driver;
-	String url;
-//	
-//	@BeforeSuite
-//	public void setup() {
-//		System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");		
-//		driver = new FirefoxDriver();
-//		driver.manage().window().maximize();
-//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-//	}
+	String url,title;
 	
-	@Test
-	public void launch() {
+	@BeforeSuite
+	public void setup() {
 		System.setProperty("webdriver.gecko.driver", "D:\\geckodriver.exe");		
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+	}
+	
+	@Test(priority =0)
+	public void launch() {
 		url = "https://www.google.com";	
 		driver.get(url);
-		System.out.println("RUNNNNNNNNNNNN");
+		System.out.println("Website opened");
 	}
+
+	@Test(priority =1)
+	public void getTtile() {
+		title = driver.getTitle();
+		Assert.assertEquals(title, "Google");
+		System.out.println("Correct title");
+	}
+	
 }
